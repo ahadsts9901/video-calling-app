9ea7f1e1a7b0174d086e1777eee542acba7402ae
ahadsts9901 <ahadsts990@gmail.com>
2024-09-12 11:51:57 +0500

    code

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,23 @@
+import { useCallback, useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Home = () => {
+  const [value, set_value] = useState("")
+
+  const navigate = useNavigate()
+
+  const handle_join_room = useCallback((e: any) => {
+    e?.preventDefault()
+    navigate(`/room/${value}`)
+  },[navigate, value])
+
   return (
-    <div>Home</div>
+    <>
+      <form onSubmit={handle_join_room}>
+        <input type="text" placeholder="enter room code" value={value} onChange={(e: any) => set_value(e?.target?.value)} />
+        <button type="submit">Join</button>
+      </form>
+    </>
   )
 }
 
--- src/pages/room/Room.tsx
+++ src/pages/room/Room.tsx
@@ -1,11 +1,35 @@
 import { useParams } from "react-router-dom"
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const Room = () => {
-
     const { roomId } = useParams()
 
+    const myMeeting = async (element: any) => {
+        const appID = 1823333989;
+        const serverSecret = "c228dabd0db884dc8009904c1d7635fb";
+        const kitToken = ZegoUIKitPrebuilt?.generateKitTokenForTest(
+            appID,
+            serverSecret,
+            `${roomId}`,
+            Date.now().toString(),
+            "new user"
+        )
+        const zc = ZegoUIKitPrebuilt.create(kitToken)
+        zc.joinRoom({
+            container: element,
+            sharedLinks: [{ name: "Copy Link", url: window?.location?.href }],
+            scenario: { mode: ZegoUIKitPrebuilt.OneONoneCall },
+            showScreenSharingButton: false,
+
+        })
+    }
+
     return (
-        <div>Room : {roomId}</div>
+        <>
+            <div>
+                <div ref={myMeeting} />
+            </div>
+        </>
     )
 }
 
