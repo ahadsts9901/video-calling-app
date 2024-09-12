daef96c446e2d4cd88ba5988f6d4f5e1f0bc72d9
ahadsts9901 <ahadsts990@gmail.com>
2024-09-12 11:20:45 +0500

    initial-setup

---
+++ src/pages/room/Room.tsx
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+
+const Room = () => {
+
+    const { roomId } = useParams()
+
+    return (
+        <div>Room : {roomId}</div>
+    )
+}
+
+export default Room
